a96648df03d36ce05a1c8a427dc88b6b5b1b4fcb
freddie <freddiechang1994@live.com>
2021-02-26 14:00:37 -0700

    completed and tested training code

--- code/main.py
+++ code/main.py
@@ -1,20 +1,19 @@
 import os
 from torchvision import transforms
-from torchvision.datasets import MNIST
-from torch.utils.data import DataLoader, random_split
 import pytorch_lightning as pl
 
-from model.LitAutoEncoder import LitAutoEncoder
+from model.SampleModel import SampleModel
+from data.salicon import SALICONDataModule
 
 from options import parser
 
 # add model specific args
-parser = LitAutoEncoder.add_model_specific_args(parser)
+parser = SampleModel.add_model_specific_args(parser)
 args = parser.parse_args()
 
-dataset = MNIST(os.getcwd(), download=True, transform=transforms.ToTensor())
-train_loader = DataLoader(dataset)
-autoencoder = LitAutoEncoder()
+data_module = SALICONDataModule(args)
+autoencoder = SampleModel()
 trainer = pl.Trainer.from_argparse_args(args)
 trainer = pl.Trainer()
-trainer.fit(autoencoder, train_loader)
+trainer.fit(autoencoder, data_module)
+trainer.test(datamodule=data_module)
